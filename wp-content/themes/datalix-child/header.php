85c7fc5e799584f30a22b9186be563643c3fc59a
Bedri Allkja <bedri.allkja@lhind.dlh.de>
2022-05-01 16:46:50 +0200

    Homepage

---
+++ wp-content/themes/datalix-child/header.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * The header.
+ *
+ * This is the template that displays all of the <head> section and everything up until main.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?> <?php twentytwentyone_the_html_classes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+    <link rel="stylesheet" href="assets/font-awesome-4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,400;0,500;0,700;1,600&display=swap"
+          rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'twentytwentyone' ); ?></a>
+
+	<?php get_template_part( 'template-parts/header/site-header' ); ?>
+
+	<div id="content" class="site-content">
+		<div id="primary" class="content-area">
+			<main id="main" class="site-main">
---
+++ wp-content/themes/datalix-child/template-parts/header/site-header.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * Displays the site header.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+
+$wrapper_classes  = 'site-header';
+$wrapper_classes .= has_custom_logo() ? ' has-logo' : '';
+$wrapper_classes .= ( true === get_theme_mod( 'display_title_and_tagline', true ) ) ? ' has-title-and-tagline' : '';
+$wrapper_classes .= has_nav_menu( 'primary' ) ? ' has-menu' : '';
+?>
+
+<!--<header id="masthead" class="--><?php //echo esc_attr( $wrapper_classes ); ?><!--">-->
+<!--	--><?php //get_template_part( 'template-parts/header/site-branding' ); ?>
+<!--	--><?php //get_template_part( 'template-parts/header/site-nav' ); ?>
+<!--</header>-->
+<!-- #masthead -->
+
+
+<div class="fluid-container main-header">
+    <div class="container custom-container">
+        <div class="row header-menu">
+            <div class="col-8 left-menu">
+                <a href="entalgo-index.html">
+                    <img src="http://localhost/datalix-website/wp-content/themes/datalix-child/assets/img/datalix.png" class="logo">
+                </a>
+                <div class="menu-items">
+                    <a href="" id="services-drop">Solutions</a>
+                    <div id="services">
+                        <a href="services/lead-generation.html" class="sub-item">
+                            <img style="width: 28px;" src="http://localhost/datalix-website/wp-content/themes/datalix-child/assets/img/search.svg">
+                            <div class="data">
+                                <div class="title">Lead Generation</div>
+                                <div class="sub-title">Lorem ipsum dolor sit amet.</div>
+                            </div>
+                        </a>
+                        <a href="services/data-entry.html" class="sub-item">
+                            <img style="width: 28px;" src="http://localhost/datalix-website/wp-content/themes/datalix-child/assets/img/insurance.svg">
+                            <div class="data">
+                                <div class="title">Data Entry</div>
+                                <div class="sub-title">Lorem ipsum dolor sit amet.</div>
+                            </div>
+                        </a>
+                        <a href="services/data-cleansing.html" class="sub-item">
+                            <img style="width: 28px;" src="http://localhost/datalix-website/wp-content/themes/datalix-child/assets/img/insurance.svg">
+                            <div class="data">
+                                <div class="title">Data Cleasing</div>
+                                <div class="sub-title">Lorem ipsum dolor sit amet.</div>
+                            </div>
+                        </a>
+                        <a href="services/outbound-automation.html" class="sub-item">
+                            <img style="width: 28px;" src="http://localhost/datalix-website/wp-content/themes/datalix-child/assets/img/insurance.svg">
+                            <div class="data">
+                                <div class="title">Outbound Automation</div>
+                                <div class="sub-title">Lorem ipsum dolor sit amet.</div>
+                            </div>
+                        </a>
+                    </div>
+                    <a href="pricing.html">Pricing</a>
+                    <a href="" id="about-drop">Why Datalix?</a>
+                    <div id="about">
+                        <div class="sub-item">
+                            <img style="width: 28px;" src="http://localhost/datalix-website/wp-content/themes/datalix-child/assets/img/insurance.svg">
+                            <div class="data">
+                                <div class="title">Careers</div>
+                                <div class="sub-title">Lorem ipsum dolor sit amet.</div>
+                            </div>
+                        </div>
+                        <a href="contact-us.html" class="sub-item">
+                            <img style="width: 28px;" src="http://localhost/datalix-website/wp-content/themes/datalix-child/assets/img/insurance.svg">
+                            <div class="data">
+                                <div class="title">Contact Us</div>
+                                <div class="sub-title">Lorem ipsum dolor sit amet.</div>
+                            </div>
+                        </a>
+                    </div>
+
+                    <!--                        <a href="">Contact</a>-->
+                    <!--                        <a href="">Join our team <span>Hiring Now!</span></a>-->
+                </div>
+            </div>
+            <div class="col right-menu">
+                <a href="" class="outline-btn">Login</a>
+                <a href="" style="margin-left: 15px;">Get Started</a>
+            </div>
+        </div>
+    </div>
+</div>
