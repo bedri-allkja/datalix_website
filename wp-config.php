e62036fe0eb579ca3ef77416d5fdeea1be740d5b
Daniel Kossakowski <daniel@cloudpanda.io>
2022-05-01 10:23:42 +0200

    Add pipeline

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'datalix-website' );
+define( 'DB_NAME', getenv('WORDPRESS_DATABASE_NAME') );
 
 /** Database username */
-define( 'DB_USER', 'datalix' );
+define( 'DB_USER', getenv('WORDPRESS_DATABASE_USER') );
 
 /** Database password */
-define( 'DB_PASSWORD', '3nta1go!Tirana' );
+define( 'DB_PASSWORD', getenv('WORDPRESS_DATABASE_PASSWORD') );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', getenv('MARIADB_HOST') );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
