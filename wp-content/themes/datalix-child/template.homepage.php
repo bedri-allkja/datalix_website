6360e85597959da337f2c60006d2b589a68d5d93
Bedri Allkja <bedri.allkja@lhind.dlh.de>
2022-05-03 22:28:23 +0200

    Content Update

--- wp-content/themes/datalix-child/template.homepage.php
+++ wp-content/themes/datalix-child/template.homepage.php
@@ -7,8 +7,8 @@
             <div class="row main-section">
                 <div class="col">
 <!--                    <div class="small-text">10x your outbound sales! <img-->
-                                src="https://stg.datalix.com/wp-content/themes/datalix-child/assets/img/flame.svg"
-                                alt=""></div>
+<!--                                src="https://stg.datalix.com/wp-content/themes/datalix-child/assets/img/flame.svg"-->
+<!--                                alt=""></div>-->
                     <div class="section-text">
                         <!--                        We provide <span>solutions</span> <br> to grow your business.-->
 <!--                        Built by <span>SDRs</span> <br> for SDRs-->
@@ -25,7 +25,7 @@
 
                     <div class="call-to-action">
                         <input type="text" placeholder="Enter your work email">
-                        <input type="submit" value="Get Started">
+                        <input type="submit" value="Free Tria">
                     </div>
 
                     <!--                    <div class="services-list">-->
@@ -99,6 +99,7 @@
                         <input type="radio" id="tab2" name="tab-control">
                         <input type="radio" id="tab3" name="tab-control">
                         <input type="radio" id="tab4" name="tab-control">
+                        <input type="radio" id="tab5" name="tab-control">
                         <ul>
                             <li title="Features">
                                 <label for="tab1" role="button">
@@ -106,10 +107,10 @@
                                         <path d="M2,10.96C1.5,10.68 1.35,10.07 1.63,9.59L3.13,7C3.24,6.8 3.41,6.66 3.6,6.58L11.43,2.18C11.59,2.06 11.79,2 12,2C12.21,2 12.41,2.06 12.57,2.18L20.47,6.62C20.66,6.72 20.82,6.88 20.91,7.08L22.36,9.6C22.64,10.08 22.47,10.69 22,10.96L21,11.54V16.5C21,16.88 20.79,17.21 20.47,17.38L12.57,21.82C12.41,21.94 12.21,22 12,22C11.79,22 11.59,21.94 11.43,21.82L3.53,17.38C3.21,17.21 3,16.88 3,16.5V10.96C2.7,11.13 2.32,11.14 2,10.96M12,4.15V4.15L12,10.85V10.85L17.96,7.5L12,4.15M5,15.91L11,19.29V12.58L5,9.21V15.91M19,15.91V12.69L14,15.59C13.67,15.77 13.3,15.76 13,15.6V19.29L19,15.91M13.85,13.36L20.13,9.73L19.55,8.72L13.27,12.35L13.85,13.36Z"/>
                                     </svg>
                                     <div class="tab-text">
-                                        <div class="tab-title">Website Analyzer</div>
+                                        <div class="tab-title">Increase Revenue</div>
                                         <div class="tab-info">
-                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aperiam
-                                            assumenda aut eligendi et nihil nulla obcaecati odit soluta voluptates!
+                                            10x your prospecting pipeline by knowing the technologies they’re running
+                                            (ex. Ecommerce platform, Analytics, Live Chat, etc.).
                                         </div>
                                     </div>
                                 </label>
@@ -120,10 +121,9 @@
                                         <path d="M2,10.96C1.5,10.68 1.35,10.07 1.63,9.59L3.13,7C3.24,6.8 3.41,6.66 3.6,6.58L11.43,2.18C11.59,2.06 11.79,2 12,2C12.21,2 12.41,2.06 12.57,2.18L20.47,6.62C20.66,6.72 20.82,6.88 20.91,7.08L22.36,9.6C22.64,10.08 22.47,10.69 22,10.96L21,11.54V16.5C21,16.88 20.79,17.21 20.47,17.38L12.57,21.82C12.41,21.94 12.21,22 12,22C11.79,22 11.59,21.94 11.43,21.82L3.53,17.38C3.21,17.21 3,16.88 3,16.5V10.96C2.7,11.13 2.32,11.14 2,10.96M12,4.15V4.15L12,10.85V10.85L17.96,7.5L12,4.15M5,15.91L11,19.29V12.58L5,9.21V15.91M19,15.91V12.69L14,15.59C13.67,15.77 13.3,15.76 13,15.6V19.29L19,15.91M13.85,13.36L20.13,9.73L19.55,8.72L13.27,12.35L13.85,13.36Z"/>
                                     </svg>
                                     <div class="tab-text">
-                                        <div class="tab-title">Bulk Analyzer</div>
+                                        <div class="tab-title">Minimize Churn</div>
                                         <div class="tab-info">
-                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aperiam
-                                            assumenda aut eligendi et nihil nulla obcaecati odit soluta voluptates!
+                                            Identify when competitor technologies are added to your customers’ websites
                                         </div>
                                     </div>
                                 </label>
@@ -134,10 +134,9 @@
                                         <path d="M2,10.96C1.5,10.68 1.35,10.07 1.63,9.59L3.13,7C3.24,6.8 3.41,6.66 3.6,6.58L11.43,2.18C11.59,2.06 11.79,2 12,2C12.21,2 12.41,2.06 12.57,2.18L20.47,6.62C20.66,6.72 20.82,6.88 20.91,7.08L22.36,9.6C22.64,10.08 22.47,10.69 22,10.96L21,11.54V16.5C21,16.88 20.79,17.21 20.47,17.38L12.57,21.82C12.41,21.94 12.21,22 12,22C11.79,22 11.59,21.94 11.43,21.82L3.53,17.38C3.21,17.21 3,16.88 3,16.5V10.96C2.7,11.13 2.32,11.14 2,10.96M12,4.15V4.15L12,10.85V10.85L17.96,7.5L12,4.15M5,15.91L11,19.29V12.58L5,9.21V15.91M19,15.91V12.69L14,15.59C13.67,15.77 13.3,15.76 13,15.6V19.29L19,15.91M13.85,13.36L20.13,9.73L19.55,8.72L13.27,12.35L13.85,13.36Z"/>
                                     </svg>
                                     <div class="tab-text">
-                                        <div class="tab-title">Salesforce Sync</div>
+                                        <div class="tab-title">Save Time</div>
                                         <div class="tab-info">
-                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aperiam
-                                            assumenda aut eligendi et nihil nulla obcaecati odit soluta voluptates!
+                                            Reduce Manual Time spent updating your CRM
                                         </div>
                                     </div>
                                 </label>
@@ -148,10 +147,23 @@
                                         <path d="M2,10.96C1.5,10.68 1.35,10.07 1.63,9.59L3.13,7C3.24,6.8 3.41,6.66 3.6,6.58L11.43,2.18C11.59,2.06 11.79,2 12,2C12.21,2 12.41,2.06 12.57,2.18L20.47,6.62C20.66,6.72 20.82,6.88 20.91,7.08L22.36,9.6C22.64,10.08 22.47,10.69 22,10.96L21,11.54V16.5C21,16.88 20.79,17.21 20.47,17.38L12.57,21.82C12.41,21.94 12.21,22 12,22C11.79,22 11.59,21.94 11.43,21.82L3.53,17.38C3.21,17.21 3,16.88 3,16.5V10.96C2.7,11.13 2.32,11.14 2,10.96M12,4.15V4.15L12,10.85V10.85L17.96,7.5L12,4.15M5,15.91L11,19.29V12.58L5,9.21V15.91M19,15.91V12.69L14,15.59C13.67,15.77 13.3,15.76 13,15.6V19.29L19,15.91M13.85,13.36L20.13,9.73L19.55,8.72L13.27,12.35L13.85,13.36Z"/>
                                     </svg>
                                     <div class="tab-text">
-                                        <div class="tab-title">Outbound Automation</div>
+                                        <div class="tab-title">Maintain Accuracy</div>
                                         <div class="tab-info">
-                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aperiam
-                                            assumenda aut eligendi et nihil nulla obcaecati odit soluta voluptates!
+                                            Keep your CRM records accurate via automation.
+                                            Avoid human error and duplication.
+                                        </div>
+                                    </div>
+                                </label>
+                            </li>
+                            <li title="Source & Validate Contacts">
+                                <label for="tab5" role="button">
+                                    <svg viewBox="0 0 24 24">
+                                        <path d="M2,10.96C1.5,10.68 1.35,10.07 1.63,9.59L3.13,7C3.24,6.8 3.41,6.66 3.6,6.58L11.43,2.18C11.59,2.06 11.79,2 12,2C12.21,2 12.41,2.06 12.57,2.18L20.47,6.62C20.66,6.72 20.82,6.88 20.91,7.08L22.36,9.6C22.64,10.08 22.47,10.69 22,10.96L21,11.54V16.5C21,16.88 20.79,17.21 20.47,17.38L12.57,21.82C12.41,21.94 12.21,22 12,22C11.79,22 11.59,21.94 11.43,21.82L3.53,17.38C3.21,17.21 3,16.88 3,16.5V10.96C2.7,11.13 2.32,11.14 2,10.96M12,4.15V4.15L12,10.85V10.85L17.96,7.5L12,4.15M5,15.91L11,19.29V12.58L5,9.21V15.91M19,15.91V12.69L14,15.59C13.67,15.77 13.3,15.76 13,15.6V19.29L19,15.91M13.85,13.36L20.13,9.73L19.55,8.72L13.27,12.35L13.85,13.36Z"/>
+                                    </svg>
+                                    <div class="tab-text">
+                                        <div class="tab-title">Source & Validate Contacts</div>
+                                        <div class="tab-info">
+                                            Find and verify contact emails are working prior to contacting them.
                                         </div>
                                     </div>
                                 </label>
@@ -162,6 +174,11 @@
                         <!--                    <div class="indicator"></div>-->
                         <!--                </div>-->
                         <div class="content">
+                            <section>
+                                <img style="height: 120%;  margin: -40px;"
+                                     src="https://stg.datalix.com/wp-content/themes/datalix-child/assets/img/bulk-email.png"
+                                     alt="">
+                            </section>
                             <section>
                                 <img style="height: 120%;  margin: -40px;"
                                      src="https://stg.datalix.com/wp-content/themes/datalix-child/assets/img/bulk-email.png"
