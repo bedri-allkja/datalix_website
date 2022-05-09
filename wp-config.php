9450c8a43117cd9e5111713355ed69dddc06f0bd
Daniel Kossakowski <daniel.kossakowski@sumup.com>
2022-05-09 11:02:47 +0200

    Enable logs

--- wp-config.php
+++ wp-config.php
@@ -1,4 +1,17 @@
 <?php
+
+// Enable WP_DEBUG mode
+define( 'WP_DEBUG', true );
+
+// Enable Debug logging to the /wp-content/debug.log file
+define( 'WP_DEBUG_LOG', true );
+
+// Disable display of errors and warnings
+define( 'WP_DEBUG_DISPLAY', true );
+@ini_set( 'display_errors', 1 );
+
+// Use dev versions of core JS and CSS files (only needed if you are modifying these core files)
+define( 'SCRIPT_DEBUG', true );
 /**
  * The base configuration for WordPress
  *
