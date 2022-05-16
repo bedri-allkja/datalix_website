4b0cc11961b13e928ff6b235309d77ddceeb6b7e
Bedri Allkja <bedri.allkja@lhind.dlh.de>
2022-05-16 23:55:11 +0200

    Remove header text

--- wp-content/themes/datalix-child/header.php
+++ wp-content/themes/datalix-child/header.php
@@ -34,7 +34,9 @@
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'twentytwentyone' ); ?></a>
+<!--	<a class="skip-link screen-reader-text" href="#content">-->
+<!--        --><?php //esc_html_e( 'Skip to content', 'twentytwentyone' ); ?>
+<!--    </a>-->
 
 	<?php get_template_part( 'template-parts/header/site-header' ); ?>
 
